02c02827dcffcc9c0ebd8ac5c581e13922081f3c
s110476-cmd <s110476@htsh.ntpc.edu.tw>
2026-04-20 11:08:24 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ function draw() {
   push();
   imageMode(CENTER);         // 設定圖片繪製基準點為中心
   translate(width / 2, height / 2); // 移至畫布中心
-  scale(1, 1);               // 改為 1, 1 以顯示真實視角（不翻轉）
+  scale(-1, 1);              // 使用 -1 啟動鏡像模式（舉左手時，畫面左邊會跟著動）
   // 繪製影像，座標設為 0, 0 是因為已經 translate 到中心點了
   image(capture, 0, 0, videoW, videoH);
   pop();
