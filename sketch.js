4135bc82fe0dae49c87d9e3afc544cd812d22a30
s110476-cmd <s110476@htsh.ntpc.edu.tw>
2026-04-20 10:38:37 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,24 +1,7 @@
-let capture;
-
 function setup() {
-  // 建立全螢幕畫布
-  createCanvas(windowWidth, windowHeight);
-  // 擷取攝影機影像
-  capture = createCapture(VIDEO);
-  capture.hide();
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background('#e9edc9'); // 設定畫布顏色為 e9edc9
-
-  // 計算顯示影像的寬高（全螢幕的 60%）
-  let vWidth = width * 0.6;
-  let vHeight = height * 0.6;
-  
-  // 置中顯示影像
-  image(capture, (width - vWidth) / 2, (height - vHeight) / 2, vWidth, vHeight);
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  background(220);
 }
